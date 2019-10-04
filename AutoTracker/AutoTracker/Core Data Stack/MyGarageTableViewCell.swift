844402e0b1fd0f6a8e4be01c7a850647b0c9f927
Sambo <flatheadcatfish9@yahoo.com>
2019-10-04 14:22:38 -0600

    Storyboards for low-fi pre meeting

---
+++ AutoTracker/AutoTracker/Core Data Stack/MyGarageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyGarageTableViewCell.swift
+//  AutoTracker
+//
+//  Created by Sam LoBue on 10/4/19.
+//  Copyright © 2019 Ian Hall. All rights reserved.
+//
+
+import UIKit
+
+class MyGarageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
