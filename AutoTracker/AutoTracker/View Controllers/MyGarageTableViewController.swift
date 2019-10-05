2331831a92012ad5d2eb6e10d085f012af67f9b6
Ian_H411 <ian.h411@gmail.com>
2019-10-04 22:54:51 -0600

    adds files and fixes missing file problem

---
+++ AutoTracker/AutoTracker/View Controllers/MyGarageTableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MyGarageTableViewController.swift
+//  AutoTracker
+//
+//  Created by Ian Hall on 10/4/19.
+//  Copyright © 2019 Ian Hall. All rights reserved.
+//
+
+import UIKit
+
+class MyGarageTableViewController: UITableViewController{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+    // MARK: - Table view data source
+
+
+
+}
+extension MyGarageTableViewController: UIViewControllerPreviewingDelegate{
+    func previewingContext(_ previewingContext: UIViewControllerPreviewing, viewControllerForLocation location: CGPoint) -> UIViewController? {
+        <#code#>
+    }
+    
+    func previewingContext(_ previewingContext: UIViewControllerPreviewing, commit viewControllerToCommit: UIViewController) {
+      
+    }
+    
+    
+    
+}
