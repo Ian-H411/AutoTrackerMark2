a2f73ae1e688611b6baba287fdc96b04cf68d2b8
Sambo <flatheadcatfish9@yahoo.com>
2019-10-07 16:17:16 -0600

    Record Tab halfway point

--- AutoTracker/AutoTracker/View Controllers/AutoTrackerButtonAsLabelStyle.swift
+++ AutoTracker/AutoTracker/View Controllers/AutoTrackerButtonAsLabelStyle.swift
@@ -15,10 +15,9 @@ class AutoTrackerButtonAsLabel: UIButton {
         addBorder(color: .black, width: 2, bg: .lightGray)
    
         addCornerRadius()
+        
     }
-   
-    
-    
+
     func addBorder(color: UIColor, width: CGFloat, bg: UIColor) {
         self.layer.borderColor = color.cgColor
         self.layer.borderWidth = width
@@ -27,5 +26,15 @@ class AutoTrackerButtonAsLabel: UIButton {
     
     func addCornerRadius() {
         self.layer.cornerRadius = frame.width / 32
+    }
+}
+
+class AutoTrackerButtonGrayBG: AutoTrackerButtonAsLabel {
+
+    override func addBorder(color: UIColor, width: CGFloat, bg: UIColor) {
+        self.layer.borderColor = color.cgColor
+        self.layer.borderWidth = width
+        self.layer.backgroundColor = UIColor.lightGray.cgColor
+        
     }
 }
---
+++ AutoTracker/AutoTracker/View Controllers/ReceiptCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ReceiptCollectionViewCell.swift
+//  AutoTracker
+//
+//  Created by Sam LoBue on 10/7/19.
+//  Copyright © 2019 Ian Hall. All rights reserved.
+//
+
+import UIKit
+
+class ReceiptCollectionViewCell: UICollectionViewCell {
+    
+}
