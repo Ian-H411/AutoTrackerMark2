bbdfffe3a3f1df610389a649d2a28726b53b8193
Ian_H411 <ian.h411@gmail.com>
2019-10-15 21:07:46 -0600

    adds hifis to the addmaintenance view controller and begins work on 2nd hald

---
+++ AutoTracker/AutoTracker/View Controllers/Maintenance/AddMaintenancePicAndPriceViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AddMaintenancePicAndPriceViewController.swift
+//  AutoTracker
+//
+//  Created by Ian Hall on 10/15/19.
+//  Copyright © 2019 Ian Hall. All rights reserved.
+//
+
+import UIKit
+
+class AddMaintenancePicAndPriceViewController: UIViewController {
+
+    var incomingMaintenance: Maintanence?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
